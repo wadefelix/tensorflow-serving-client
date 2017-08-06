aaf1af4038fea6c0bcc64a779dcb7468443a8119
Steven Normore <snormore@gmail.com>
2017-08-06 19:13:27 -0400

    Remove high level classify abstraction and image processing from this client

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,36 @@
+import numpy as np
+
+from PIL import Image
 from tensorflow_serving_client import TensorflowServingClient
 
 
-def test_classify_image():
-    client = TensorflowServingClient('localhost', 9000, 'mobilenet_v1-1', (224, 224))
-    result = client.classify_image('tests/fixtures/files/cat.jpg')
-    assert len(result) > 0
+def preprocess_input(x):
+    x /= 255.
+    x -= 0.5
+    x *= 2.
+    return x
+
+
+def load_image(image_path, target_size=None):
+    img = Image.open(image_path)
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    if target_size:
+        width_height = (target_size[0], target_size[1])
+        if img.size != width_height:
+            img = img.resize(width_height)
+    image_data = np.asarray(img, dtype=np.float32)
+    image_data = preprocess_input(image_data)
+    image_data = np.expand_dims(image_data, axis=0)
+    return image_data
+
+
+def test_client_make_prediction():
+    client = TensorflowServingClient('localhost', 9000)
+    image = load_image('tests/fixtures/files/cat.jpg', (224, 224))
+    result = client.make_prediction(image, 'image')
+    assert 'class_probabilities' in result
+    assert len(result['class_probabilities']) == 1
+    assert len(result['class_probabilities'][0]) == 7
+    assert len(result['class_probabilities'][0][0]) == 7
+    assert len(result['class_probabilities'][0][0][0]) == 1024
