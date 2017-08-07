5d10c06ecd875bfae789b9f5d60e3c7d80738c67
Steven Normore <snormore@gmail.com>
2017-08-07 00:45:31 -0400

    Bump version in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ class BuildPackageProtos(install):
 
 setup(
     name='tensorflow_serving_client',
-    version='0.0.4',
+    version='0.0.5',
     description='Python client for tensorflow serving',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
