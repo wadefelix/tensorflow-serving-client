fb39525eb0ba6619ee3f88f07312b9b86957bbd4
Steven Normore <snormore@gmail.com>
2017-08-20 18:22:38 -0400

    Test a few Python versions in CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,11 @@
 import pytest
 import csv
-import codecs
-
-from urllib.request import urlopen
+import requests
 
 
 @pytest.fixture
 def imagenet_dictionary():
-    response = urlopen('https://s3.amazonaws.com/tf-models-839c7ddd-9cab-49fa-9b42-bde1a842086e/dictionary.csv')
-    reader = csv.reader(codecs.iterdecode(response, 'utf-8'))
+    response = requests.get('https://s3.amazonaws.com/tf-models-839c7ddd-9cab-49fa-9b42-bde1a842086e/dictionary.csv')
+    reader = csv.reader(response.text.splitlines())
     dictionary = dict(list(reader))
     return [dictionary[key] for key in sorted(dictionary)]
