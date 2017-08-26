8d2e2395ba322d5e63bfedd29a786a9389ba8e8a
Steven Normore <snormore@gmail.com>
2017-08-25 20:47:01 -0400

    Session scope the pytest imagenet_dictionary fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import csv
 import requests
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def imagenet_dictionary():
     response = requests.get('https://s3.amazonaws.com/tf-models-839c7ddd-9cab-49fa-9b42-bde1a842086e/dictionary.csv')
     reader = csv.reader(response.text.splitlines())
