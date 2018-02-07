a7d67ccb3fe6137bc91d50fd111f3d2d283f8e29
Steven Normore <snormore@gmail.com>
2018-02-07 01:04:47 -0500

    Use non beta grpc code in client.py

--- tensorflow_serving_client/client.py
+++ tensorflow_serving_client/client.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
+import grpc
 
-from grpc.beta import implementations
-from tensorflow_serving_client.protos import prediction_service_pb2, predict_pb2
+from tensorflow_serving_client.protos import prediction_service_pb2_grpc, predict_pb2
 from tensorflow_serving_client.proto_util import copy_message
 
 
@@ -10,8 +10,8 @@ class TensorflowServingClient(object):
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.channel = implementations.insecure_channel(self.host, self.port)
-        self.stub = prediction_service_pb2.beta_create_PredictionService_stub(self.channel)
+        self.channel = grpc.insecure_channel('%s:%s' % (host, port))
+        self.stub = prediction_service_pb2_grpc.PredictionServiceStub(self.channel)
 
     def execute(self, request, timeout=10.0):
         return self.stub.Predict(request, timeout)
